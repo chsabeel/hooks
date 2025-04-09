a36cfd713a427648326d7158f18e466728bfc8f4
ch sabeel <sabeel446@gmail.com>
2025-04-09 16:48:51 +0500

    use stream

--- lib/home.dart
+++ lib/home.dart
@@ -4,32 +4,20 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 class Home extends HookWidget {
   const Home({super.key});
 
-  // Stream<String> getTime() {
-  //   return Stream.periodic(Duration(seconds: 1), (_) {
-  //     return DateTime.now().toString().substring(0, 19);
-  //   });
-  // }
+  Stream<String> getTime() {
+    return Stream.periodic(Duration(seconds: 1), (_) {
+      return DateTime.now().toString().substring(0, 19);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState("");
-
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text.length.toString();
-      });
-    }, [controller]);
-
+    final dateTime = useStream(getTime());
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(controller: controller),
-            Text("You Typed ${text.value}"),
-          ],
-        ),
+      appBar: AppBar(title: Text("")),
+      body: ElevatedButton(
+        onPressed: () {},
+        child: Text(dateTime.data ?? "null"),
       ),
     );
   }
